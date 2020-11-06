50dfa7fc192a0781050e6a42f306dad2625e8ccf
kawada.kyosuke.mc00359@memberscareer.co.jp <kawada.kyosuke.mc00359@memberscareer.co.jp>
2020-11-06 09:45:00 +0900

    履歴ボタン作成

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import { render } from 'react-dom'
 import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components'
 import { Editor } from './pages/editor'
+import { History } from './pages/history'
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
 const GlobalStyle = createGlobalStyle`
 body * {
@@ -13,7 +21,15 @@ body * {
 const Main = (
 <>
   <GlobalStyle />
-  <Editor />
+  <Router>
+    <Route exact path="/editor">
+      <Editor />
+    </Route>
+    <Route exact path="/history">
+      <History />
+    </Route>
+    <Redirect to="/editor" path="*" />
+  </Router>
 </>
 )
 
